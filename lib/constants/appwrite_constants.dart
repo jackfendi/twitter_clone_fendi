0c8aaf578ecb9c8f72bf9ef27af0199a3e97d93e
jackfendi <syahrulfendi44@gmail.com>
2023-07-11 13:17:23 +0700

    pertama commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6417cbbbab7b226e3dd4';
+  static const String projectId = '6417be3a2ddd9b9d59a6';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
